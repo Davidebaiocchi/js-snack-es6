28b259cb72c476f3b49598cf4d742e144fb5cbb0
davide <davide.baiocchi97@gmail.com>
2021-06-18 13:26:18 +0200

    init

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ console.log(lightBike);
 let {nome, peso} = lightBike;
 console.log(nome, peso);
 
-// stampo con template literal:l 
+// stampo con template literal: 
 lightBike = `
 <h2>Nome: ${nome}</h2>
 <h2>Peso: ${peso}</h2>
@@ -94,4 +94,11 @@ const team = [
      nuovoArray.push(newTeam);
  }
  console.log(nuovoArray);
+
+
+
+//  Snack 3
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+// Usiamo i nuovi metodi degli array foreach o filter
 
